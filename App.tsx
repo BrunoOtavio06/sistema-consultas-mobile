9f205c5a721aebee87b1372c6680615b7fb59c89
BrunoOtavio06 <bruno.o.carvalho06@gmail.com>
2026-03-13 09:28:00 -0300

    feat: install web depedencies and create a test script in App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Finalmente eu tenho um iPhone</Text>
+      <Text style={styles.subtitle}>João Limão hahahaha</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'light',
+  },
+  subtitle: {
+    color: '#d0ff84',
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
 });
